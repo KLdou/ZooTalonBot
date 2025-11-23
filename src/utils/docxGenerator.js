26cd0b97e732b08c700817c24ee1d5b9a8c4859e
Kanstantsin <kostice@tut.by>
2025-11-23 17:55:43 +0300

    Added transliteration

--- src/utils/docxGenerator.js
+++ src/utils/docxGenerator.js
@@ -4,6 +4,24 @@ const PizZip = require("pizzip");
 const { DOMParser } = require("@xmldom/xmldom");
 const { log, logError } = require("./helpers");
 
+// Функция транслитерации кириллицы в латиницу
+function transliterate(text) {
+  const translitMap = {
+    'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo',
+    'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm',
+    'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u',
+    'ф': 'f', 'х': 'h', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'sch',
+    'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya',
+    'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E', 'Ё': 'Yo',
+    'Ж': 'Zh', 'З': 'Z', 'И': 'I', 'Й': 'Y', 'К': 'K', 'Л': 'L', 'М': 'M',
+    'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U',
+    'Ф': 'F', 'Х': 'H', 'Ц': 'Ts', 'Ч': 'Ch', 'Ш': 'Sh', 'Щ': 'Sch',
+    'Ъ': '', 'Ы': 'Y', 'Ь': '', 'Э': 'E', 'Ю': 'Yu', 'Я': 'Ya'
+  };
+
+  return text.split('').map(char => translitMap[char] || char).join('');
+}
+
 function removeFileAfterSend(filePath) {
   fs.unlink(filePath, (err) => {
     if (err) logError(`Ошибка удаления файла ${filePath}`, err);
@@ -13,6 +31,9 @@ function removeFileAfterSend(filePath) {
 
 // Упрощённый интерфейс для использования
 async function openAndReplacePlaceholders(inputFilename, outputFilename, data) {
+  // Транслитерация имени файла для совместимости с системами без поддержки кириллицы
+  outputFilename = transliterate(outputFilename);
+  
   if (!(await checkFile(outputFilename))) {
     null;
   }
@@ -62,4 +83,5 @@ async function checkFile(path) {
 module.exports = {
   removeFileAfterSend,
   openAndReplacePlaceholders,
+  transliterate,
 };
