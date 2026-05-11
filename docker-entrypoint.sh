db1d7ad1b74376134c4213dfa5de85d3d3be860d
Kanstantsin <kostice@tut.by>
2026-05-11 17:58:34 +0300

    added documents folder to container

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,9 +2,12 @@
 set -e
 
 mkdir -p /app/logger
+mkdir -p /app/documents
 touch /app/logger/zoo-bot.log
 chown -R node:node /app/logger
+chown -R node:node /app/documents
 chmod 775 /app/logger
+chmod 775 /app/documents
 chmod 664 /app/logger/zoo-bot.log
 
 exec su-exec node "$@"
